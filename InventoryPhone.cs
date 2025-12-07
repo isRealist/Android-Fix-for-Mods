308c452745bf94de8552fc7f7e12ddc3ba3ef0d2
isRealist <hamdudumiedie@gmail.com>
2025-12-08 06:59:51 +0700

    Update InventoryPhone.cs

--- InventoryPhone.cs
+++ InventoryPhone.cs
@@ -2,7 +2,6 @@ using System.Linq;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.Objects;
 
 namespace MiniPhone.Inventory
 {
@@ -21,7 +20,10 @@ namespace MiniPhone.Inventory
         {
             return Context.IsWorldReady &&
                    Game1.player?.Items != null &&
-                   Game1.player.Items.Any(i => i is Furniture f && f.ParentSheetIndex == 3490);
+                   Game1.player.Items.Any(i =>
+                       i is StardewValley.Objects.Furniture f &&
+                       f.ParentSheetIndex == 3490
+                   );
         }
 
         private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
@@ -29,15 +31,15 @@ namespace MiniPhone.Inventory
             if (!HasPhoneInInventory() || !Context.IsPlayerFree)
                 return;
 
-            int ticks = mod.Config.CheckIntervalSeconds * 60;
+            int ticks = MiniPhoneMod.Instance.Config.CheckIntervalSeconds * 60;
             counter++;
 
             if (counter >= ticks)
             {
                 counter = 0;
 
-                if (Game1.random.Next(100) < mod.Config.RandomCallChance)
-                    mod.Calls.TriggerRandomCall();
+                if (Game1.random.Next(100) < MiniPhoneMod.Instance.Config.RandomCallChance)
+                    MiniPhoneMod.Instance.Calls.TriggerRandomCall();
             }
         }
     }
